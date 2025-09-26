7130b8c69bb3524adcb869472a313bc2406a5e34
Moch Zawaruddin Abdullah <zawaruddin@polinema.ac.id>
2025-09-26 08:50:14 +0700

    Jobsheet 05

---
+++ JS05_PHP2/test.php
@@ -0,0 +1,10 @@
+<?php 
+
+date_default_timezone_set('Asia/Jakarta');
+echo 'WIB : ' . date('Y-F-d H:i:s').' <br>';
+
+date_default_timezone_set('Asia/Makassar');
+echo 'WITA : ' . date('Y-m-d H:i:s').' <br>';
+
+date_default_timezone_set('Asia/Jayapura');
+echo 'WIT : ' . date('Y-m-d H:i:s');
